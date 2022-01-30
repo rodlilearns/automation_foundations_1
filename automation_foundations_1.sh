48aa23c63b28f9a8a5eced801ff3498ce0c89b57
rodli <rodli@redhat.com>
2022-01-30 16:24:14 +1100

    update: added secret prompt

--- automation_foundations_1.sh
+++ automation_foundations_1.sh
@@ -2,7 +2,7 @@
 echo "Type your rhsm username"
 read rhsm_username
 echo "Type your rhsm password"
-read rhsm_password
+read -s rhsm_password
 echo "Type your pool id"
 read pool_id
 sudo subscription-manager register --username=$rhsm_username --password=$rhsm_password
