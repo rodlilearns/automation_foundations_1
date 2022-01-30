120fb32fb853e37e3c286a0a2d70ff808e08d334
rodli <rodli@redhat.com>
2022-01-30 16:18:53 +1100

    update: added prompts

--- automation_foundations_1.sh
+++ automation_foundations_1.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-rhsm_username=
-rhsm_password=
-pool_id=
+echo "Type your rhsm username"
+read rhsm_username
+echo "Type your rhsm password"
+read rhsm_password
+echo "Type your pool id"
+read pool_id
 sudo subscription-manager register --username=$rhsm_username --password=$rhsm_password
 sudo subscription-manager attach --pool=$pool_id
 sudo subscription-manager repos --enable=rhel-8-for-x86_64-baseos-rpms --enable=rhel-8-for-x86_64-appstream-rpms --enable=ansible-automation-platform-2.1-for-rhel-8-x86_64-rpms
