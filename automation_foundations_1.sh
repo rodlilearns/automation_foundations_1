66b7bcef1515dcfe87cd17bb1d0285fedbe6809f
rodli <rodli@redhat.com>
2022-01-31 08:01:36 +1100

    update: fixed variable input features. complex passwords require backspaces

--- automation_foundations_1.sh
+++ automation_foundations_1.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-echo "Type your rhsm username"
-read rhsm_username
-echo "Type your rhsm password"
-read -s rhsm_password
-echo "Type your pool id"
-read pool_id
+read -p "Enter your rhsm username: " rhsm_username
+read -sp "Enter your rhsm password: " rhsm_password
+read -p "Enter your pool id: " pool_id
 sudo subscription-manager register --username=$rhsm_username --password=$rhsm_password
 sudo subscription-manager attach --pool=$pool_id
 sudo subscription-manager repos --enable=rhel-8-for-x86_64-baseos-rpms --enable=rhel-8-for-x86_64-appstream-rpms --enable=ansible-automation-platform-2.1-for-rhel-8-x86_64-rpms
