75e377c83ab72f96ebe21b5d3dc10813e6e3578e
rodli <86593613+rodlilearns@users.noreply.github.com>
2022-05-22 20:59:42 +1000

    Refactor: Added disable all repos step

--- automation_foundations_1.sh
+++ automation_foundations_1.sh
@@ -4,5 +4,6 @@ read -sp "Enter your rhsm password: " rhsm_password
 read -p "Enter your pool id: " pool_id
 sudo subscription-manager register --username=$rhsm_username --password=$rhsm_password
 sudo subscription-manager attach --pool=$pool_id
+sudo subscription-manager repos --disable="*"
 sudo subscription-manager repos --enable=rhel-8-for-x86_64-baseos-rpms --enable=rhel-8-for-x86_64-appstream-rpms --enable=ansible-automation-platform-2.1-for-rhel-8-x86_64-rpms
 sudo dnf install -y ansible
